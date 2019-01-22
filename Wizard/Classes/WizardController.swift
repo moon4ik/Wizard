3773b0c23a093ee10000e55e33381d78d48f815a
Oleksii Mykhailenko <moon4ik@gmail.com>
2019-01-22 16:53:34 +0200

    Fix rotation and page position. Need to fix image proportion.

--- Wizard/Classes/WizardController.swift
+++ Wizard/Classes/WizardController.swift
@@ -105,6 +105,20 @@ public class WizardController: UICollectionViewController, UICollectionViewDeleg
         dismiss(animated: false, completion: nil)
     }
     
+    public override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (_) in
+            self.collectionViewLayout.invalidateLayout()
+            if self.pageControl.currentPage == 0 {
+                self.collectionView.contentOffset = .zero
+            } else {
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+        }) { (_) in
+            
+        }
+    }
+    
     // MARK: - ScrollView
     
     public override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
